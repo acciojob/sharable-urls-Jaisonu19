962fe3729a5572bc7cd43a2dfa3b5766e3fe70fb
Jaisonu19 <134277404+Jaisonu19@users.noreply.github.com>
2023-12-29 14:26:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+function updateurl() {
+	//get the value from input fields
+	var name=document.getElementById("name").value;
+	var year=document.getElementById("year").value;
+	//udate the url with the appropriate query string.
+	var urlString="https://localhost:8080/?name="+name+"&year="+year;
+	// Update the text in the h3 element
+	document.getElementById("url").textContent = urlString;
+	
+}
